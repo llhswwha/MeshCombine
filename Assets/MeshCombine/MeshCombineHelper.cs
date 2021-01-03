9cbe0bff96ea7410b812a9ac0fb124ecf6a43ec3
caiwanwei <llhswwha@gmail.com>
2021-01-04 06:34:22 +0800

    加载子场景

---
+++ Assets/MeshCombine/MeshCombineHelper.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public static class MeshCombineHelper
+{
+
+    public static GameObject SimpleCombine(GameObject source,GameObject target){
+        
+        CombinedMesh combinedMesh=new CombinedMesh(source.transform,null,null);
+        combinedMesh.DoCombine(true);
+        target=combinedMesh.CreateNewGo(false,target);
+        target.AddComponent<MeshInfo>();
+        Debug.Log("Combine:"+source+"->"+target);
+        return target;
+    }
+
+    public static IEnumerator SimpleCombine_Coroutine(GameObject source,GameObject target,int waitCount,bool isDestroy,Action<GameObject> callback){
+        
+        CombinedMesh combinedMesh=new CombinedMesh(source.transform,null,null);
+        yield return combinedMesh.DoCombine_Coroutine(true,waitCount);
+        target=combinedMesh.CreateNewGo(false,target);
+        target.AddComponent<MeshInfo>();
+        Debug.Log("Combine:"+source+"->"+target);
+        if(isDestroy){
+            GameObject.Destroy(source);
+        }
+        if(callback!=null){
+            callback(source);
+        }
+        yield return target;
+    }
+    
+    public static GameObject CombineMaterials(GameObject go,out int  count){
+        DateTime start=DateTime.Now;
+        GameObject goNew=new GameObject();
+        goNew.name=go.name+"_Combined";
+        goNew.transform.SetParent(go.transform.parent);
+        //int count=0;
+        Dictionary<Material,List<MeshFilter>> mat2Filters=GetMatFilters(go,out count);
+        foreach(var item in mat2Filters)
+        {
+            Material material=item.Key;
+            List<MeshFilter> list=item.Value;
+
+            CombinedMesh combinedMesh=new CombinedMesh(go.transform,list,material);
+            combinedMesh.DoCombine(true);
+            GameObject matGo=combinedMesh.CreateNewGo(false,null);
+            matGo.name=material.name;
+            matGo.transform.SetParent(goNew.transform);
+        }
+        Debug.LogError(string.Format("CombineMaterials 用时:{0},Mat数量:{1},Mesh数量:{2}",(DateTime.Now-start),mat2Filters.Count,count));
+        return goNew;
+    }
+
+     public static IEnumerator CombineMaterials_Coroutine(GameObject go,int waitCount,bool isDestroy,Action<GameObject> callback){
+        DateTime start=DateTime.Now;
+        GameObject goNew=new GameObject();
+        goNew.name=go.name+"_Combined";
+        goNew.transform.SetParent(go.transform.parent);
+        int count=0;
+        Dictionary<Material,List<MeshFilter>> mat2Filters=GetMatFilters(go,out count);
+        yield return null;
+        int i=0;
+        foreach(var item in mat2Filters)
+        {
+            Material material=item.Key;
+            List<MeshFilter> list=item.Value;
+            Debug.LogWarning(string.Format("CombineMaterials_Coroutine {0} ({1}/{2})",material,i+1,mat2Filters.Count));
+            CombinedMesh combinedMesh=new CombinedMesh(go.transform,list,material);
+            yield return combinedMesh.DoCombine_Coroutine(true,waitCount);
+            GameObject matGo=combinedMesh.CreateNewGo(false,null);
+            matGo.name=material.name;
+            matGo.transform.SetParent(goNew.transform);
+            yield return goNew;
+            i++;
+        }
+        if(isDestroy){
+            GameObject.Destroy(go);
+        }
+        Debug.LogError(string.Format("CombineMaterials 用时:{0},Mat数量:{1},Mesh数量:{2}",(DateTime.Now-start),mat2Filters.Count,count));
+        if(callback!=null){
+            callback(goNew);
+        }
+        yield return goNew;
+    }
+   
+    public static Dictionary<Material,List<MeshFilter>> GetMatFilters(GameObject go,out int count){
+        DateTime start=DateTime.Now;
+        Dictionary<Material,List<MeshFilter>> mat2Filters=new Dictionary<Material, List<MeshFilter>>();
+        MeshRenderer[] renderers=go.GetComponentsInChildren<MeshRenderer>();
+        count=renderers.Length;
+        for(int i=0;i<renderers.Length;i++){
+            MeshRenderer renderer=renderers[i];
+            if(!mat2Filters.ContainsKey(renderer.sharedMaterial)){
+                mat2Filters.Add(renderer.sharedMaterial,new List<MeshFilter>());
+            }
+            List<MeshFilter> list=mat2Filters[renderer.sharedMaterial];
+            MeshFilter filter=renderer.GetComponent<MeshFilter>();
+            list.Add(filter);
+        }
+        Debug.LogError(string.Format("GetMatFilters 用时:{0},Mat数量:{1},Mesh数量:{2}",(DateTime.Now-start),mat2Filters.Count,count));
+        return mat2Filters;
+    }
+
+    public static GameObject Combine(GameObject source){
+        DateTime start=DateTime.Now;
+        int count=0;
+        GameObject goNew=CombineMaterials(source,out count);//按材质合并
+        CombinedMesh combinedMesh=new CombinedMesh(goNew.transform,null,null);
+        combinedMesh.DoCombine(false);
+        GameObject target=combinedMesh.CreateNewGo(false,null);
+        target.name=source.name+"_Combined";
+        goNew.transform.SetParent(target.transform);
+        GameObject.DestroyImmediate(goNew);
+        Debug.LogError(string.Format("CombinedMesh 用时:{0}ms,数量:{1}",(DateTime.Now-start).TotalMilliseconds,count));
+        return target;
+    }
+
+    public static IEnumerator Combine_Coroutine(GameObject source,int waitCount,bool isDestroy,Action<GameObject> callback){
+        DateTime start=DateTime.Now;
+        int count=0;
+        GameObject goNew=CombineMaterials(source,out count);//按材质合并
+        CombinedMesh combinedMesh=new CombinedMesh(goNew.transform,null,null);
+        yield return combinedMesh.DoCombine_Coroutine(false,waitCount);
+        GameObject target=combinedMesh.CreateNewGo(false,null);
+        target.name=source.name+"_Combined";
+        goNew.transform.SetParent(target.transform);
+        GameObject.DestroyImmediate(goNew);
+        Debug.LogError(string.Format("CombinedMesh 用时:{0}ms,数量:{1}",(DateTime.Now-start).TotalMilliseconds,count));
+        if(isDestroy)
+        {
+            GameObject.Destroy(source);
+        }
+        if(callback!=null){
+            callback(target);
+        }
+        yield return target;
+    }
+
+    public static GameObject CombineEx(GameObject source,int mode=0){
+        GameObject result=null;
+        if(mode==0){
+            result= Combine(source);
+        }
+        else if(mode ==1 )
+        {
+            int count=0;
+            result= CombineMaterials(source,out count);
+        }
+        else{
+            result= SimpleCombine(source,null);
+        }
+        return result;
+    }
+
+    public static IEnumerator CombineEx_Coroutine(GameObject source,bool isDestroy,int waitCount,int mode,Action<GameObject> callback){
+        if(mode==0){
+            yield return Combine_Coroutine(source,waitCount,isDestroy,callback);
+        }
+        else if(mode ==1 )
+        {
+            yield return CombineMaterials_Coroutine(source,waitCount,isDestroy,callback);
+        }
+        else{
+           yield return SimpleCombine_Coroutine(source,null,waitCount,isDestroy,callback);
+        }
+    }
+
+    public static Dictionary<GameObject,CombinedMesh> go2ms=new Dictionary<GameObject, CombinedMesh>();
+    public static void AddGo(GameObject gameObject,CombinedMesh mesh){
+        if(go2ms.ContainsKey(gameObject)){
+            go2ms[gameObject]=mesh;
+        }
+        else{
+            go2ms.Add(gameObject,mesh);
+        }
+        
+    }
+
+    public static CombinedMesh GetMesh(GameObject gameObject){
+        if(go2ms.ContainsKey(gameObject)){
+            return go2ms[gameObject];
+        }
+        else{
+            return null;
+        }
+    }
+
+    public static void RemveGo(GameObject gameObject){
+        CombinedMesh mesh=GetMesh(gameObject);
+        if(mesh!=null){
+            Debug.Log("mesh:"+mesh.name);
+            Debug.Log("mesh.meshFilters:"+mesh.meshFilters.Count);
+            MeshFilter[] meshFilters=gameObject.GetComponentsInChildren<MeshFilter>();
+            foreach(var mf in meshFilters){
+                mesh.meshFilters.Remove(mf);
+            }
+            MeshRenderer[] meshRenderers=gameObject.GetComponentsInChildren<MeshRenderer>();
+            foreach(var mr in meshRenderers){
+                mr.enabled=true;
+                mr.material.SetColor("_BaseColor",Color.red);
+            }
+            Debug.Log("mesh.meshFilters:"+mesh.meshFilters.Count);
+            mesh.Refresh();//重新合并
+        }
+        else{
+            Debug.LogError("未找到CombinedMesh:"+gameObject);
+        }
+    }
+
+}
--- Assets/MeshCombine/MeshCombiner.cs
+++ Assets/MeshCombine/MeshCombiner.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System;
-
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
 //[RequireComponent(typeof(MeshRenderer),typeof(MeshFilter))]
 public class MeshCombiner : MonoBehaviour
 {
@@ -30,17 +33,20 @@ public class MeshCombiner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(Auto)
+        {
+            CombineByMaterial();
+        }
+
+        LoadScenes();
+    }
+
+    private void InitSetting(){
         if(Setting==null){
             Setting=MeshCombinerSetting.Instance;
         }
         if(Setting==null){
             Setting=this.gameObject.AddComponent<MeshCombinerSetting>();
-        }
-
-
-        if(Auto)
-        {
-            CombineByMaterial();
         }
     }
 
@@ -57,8 +63,14 @@ public class MeshCombiner : MonoBehaviour
 
     public List<MeshCombiner> SubCombiners=new List<MeshCombiner>();
 
+    public bool IsSaveToScene=false;
+
+    public List<string> modelScenes=new List<string>();
+
     private void CombineEx(int mode)
     {
+        InitSetting();
+
         Debug.LogError("Start CombineEx mode:"+mode+"|"+gameObject);
         resultList=new List<GameObject>();
         string sourceName="";
@@ -124,7 +136,13 @@ public class MeshCombiner : MonoBehaviour
         // }
 
         if(Setting.IsCoroutine){
-            StartCoroutine(CombineEx_Coroutine(mode));
+            //合并
+            StartCoroutine(CombineEx_Coroutine(mode,t=>{
+                Debug.Log("Result:"+t);
+                if(IsSaveToScene){
+                    SaveToScene(t);
+                }
+            }));
         }
         else{
             for (int i = 0; i < sourceList.Count; i++)
@@ -149,19 +167,90 @@ public class MeshCombiner : MonoBehaviour
                     // }
                     // else
                     {
-                        GameObject target=MeshCombineHelper.CombineEx(source,mode);
+                        GameObject target=MeshCombineHelper.CombineEx(source,mode);//合并
                         resultList.Add(target);
                         Debug.Log("Combine:"+source+"->"+target);
                         if(Setting.IsDestroySource){
                             GameObject.Destroy(source);
                         }
+                        // Scene scene=SceneManager.CreateScene(target.name);
+                        // SceneManager.MoveGameObjectToScene(target, scene);
+                        if(IsSaveToScene){
+                            SaveToScene(target);
+                        }
+                        
                     }
 
                 }
             }
         }
     }
-    private IEnumerator CombineEx_Coroutine(int mode)
+
+    private void SaveToScene(GameObject target){
+        #if UNITY_EDITOR
+        Scene scene=EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
+        scene.name=target.name;
+        target.transform.SetParent(null);
+        SceneManager.MoveGameObjectToScene(target, scene);
+        Debug.LogError("path:"+(EditorSceneManager.GetActiveScene().path));
+        //string[] path = EditorSceneManager.GetActiveScene().path.Split(char.Parse("/"));
+        //path[path.Length - 1] = "AutoSave_" + path[path.Length - 1];
+        //bool saveOK = EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), string.Join("/", path));
+        string path="Assets\\CombinedScene\\"+target.name+".unity";
+        EditorSceneManager.SaveScene(scene,path);  
+        modelScenes.Add(path);   
+        #endif
+    }
+
+    private void LoadScenes(){
+        StartCoroutine(LoadYourAsyncScenes());
+    }
+
+    IEnumerator LoadYourAsyncScenes()
+    {
+        foreach(string path in modelScenes)
+        {
+            if(string.IsNullOrEmpty(path))continue;
+            Debug.Log("path:"+path);
+            Scene scene=SceneManager.GetSceneByPath(path);
+            if(scene==null)continue;
+            string sceneName=path.Replace("Assets\\","");//Assets\CombinedScene\Group1 (1)_Combined.unity
+            sceneName=sceneName.Replace("\\","/");
+            sceneName=sceneName.Replace(".unity","");
+            //SceneManager.LoadSceneAsync(scene.name);
+            Debug.Log("load:"+scene.name+"|"+sceneName);
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName,LoadSceneMode.Additive);
+            // Wait until the asynchronous scene fully loads
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+        }
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+
+    }
+
+    IEnumerator LoadYourAsyncScene(string sceneName)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    private IEnumerator CombineEx_Coroutine(int mode,Action<GameObject> callback)
     {
         DateTime start=DateTime.Now;
         for (int i = 0; i < sourceList.Count; i++)
@@ -170,7 +259,7 @@ public class MeshCombiner : MonoBehaviour
              Debug.LogError(string.Format("CombineEx {0} ({1}/{2})",source,i+1,sourceList.Count));
             if (source!=null)
             {
-                yield return MeshCombineHelper.CombineEx_Coroutine(source,Setting.IsDestroySource,Setting.WaitCount,mode);
+                yield return MeshCombineHelper.CombineEx_Coroutine(source,Setting.IsDestroySource,Setting.WaitCount,mode,callback);
             }
         }
         Setting.WriteLog("完成合并 用时:"+(DateTime.Now-start));
